1962565899455555efee2f028ce452d5799ccae4
Amr Saeed <amrsaeed1899@gmail.com>
2026-06-29 22:59:51 +0300

    Adding oneSignal

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import OneSignal from "react-onesignal";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.jsx";
+import "./index.css";
+
+async function startApp() {
+  await OneSignal.init({
+    appId: "00c6284a-0f9c-42b7-916d-e615df732aa0",
+
+    // Remove this after deploying if you want.
+    allowLocalhostAsSecureOrigin: true,
+  });
+
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
+
+startApp();
